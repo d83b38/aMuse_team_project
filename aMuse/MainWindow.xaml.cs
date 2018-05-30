d51c1f07c0859e68e85577df9772172920bf15cd
Щучкин Илья <f99990000@gmail.com>
2018-05-31 02:55:44 +0300

    Added player and basic markup

--- aMuse/MainWindow.xaml.cs
+++ aMuse/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Vlc.DotNet.Wpf;
+using Vlc.DotNet.Core;
+using System.IO;
 
 namespace aMuse
 {
@@ -20,9 +23,20 @@ namespace aMuse
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool IsPaused { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            vlcPlayer.MediaPlayer.VlcLibDirectory = new DirectoryInfo("libvlc/win-x86");
+            vlcPlayer.MediaPlayer.EndInit();
+        }
+
+        private void PlayPause_Click(object sender, RoutedEventArgs e) {
+            if (IsPaused == false) {
+                vlcPlayer.MediaPlayer.Play(new FileInfo("track.mp3"));
+                IsPaused = true;
+            }
+            vlcPlayer.MediaPlayer.VlcMediaPlayer.Pause();
         }
     }
 }
