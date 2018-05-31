777aea5654101d994662c067d9e680e25917bd55
Даниил Бабич <dbabich@edu.hse.ru>
2018-05-31 20:24:22 +0300

    Completed Migration to PackageReference (read more about it here https://docs.microsoft.com/en-us/nuget/reference/migrate-packages-config-to-package-reference ) Added simple page/elements of UI Added Icons folder Added and edited buttons (play/pause icons can dynamically change) Added resourse dictionary for testing Added song ang album cover for testing

--- aMuse/MainWindow.xaml.cs
+++ aMuse/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using Vlc.DotNet.Wpf;
 using Vlc.DotNet.Core;
 using System.IO;
+using aMuse.UI;
 
 namespace aMuse
 {
@@ -23,20 +24,44 @@ namespace aMuse
     /// </summary>
     public partial class MainWindow : Window
     {
+
         private bool IsPaused { get; set; }
         public MainWindow()
         {
+            IsPaused = true;
             InitializeComponent();
             vlcPlayer.MediaPlayer.VlcLibDirectory = new DirectoryInfo("libvlc/win-x86");
             vlcPlayer.MediaPlayer.EndInit();
         }
 
         private void PlayPause_Click(object sender, RoutedEventArgs e) {
-            if (IsPaused == false) {
+            if (IsPaused == true) {
                 vlcPlayer.MediaPlayer.Play(new FileInfo("track.mp3"));
-                IsPaused = true;
+                imageInside.Source = new BitmapImage(new Uri("pack://application:,,,/Icons/Pause_52px.png"));
+                IsPaused = false;
             }
-            vlcPlayer.MediaPlayer.VlcMediaPlayer.Pause();
+            else
+            {
+                vlcPlayer.MediaPlayer.VlcMediaPlayer.Pause();
+                IsPaused = true;
+                imageInside.Source = new BitmapImage(new Uri("pack://application:,,,/Icons/Play_52px.png"));
+            }
+
+        }
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MainFrame.Content = new MainPage();
+
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Previous_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
